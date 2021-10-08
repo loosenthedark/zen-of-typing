07812053acd311c370058e658b174a4b79aeda44
Paul Harrington <hello@loosenthedark.tech>
2021-10-08 21:53:51 +0100

    Create texts dir and add five text files

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-print('Hello, Python!')
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+def choose_text(self):
+    pass
+
+if __name__ == '__main__':
+    pass # test code
