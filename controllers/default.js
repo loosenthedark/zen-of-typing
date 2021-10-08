616d47176ea1ef237246fd57e28b1ce089708b45
Paul Harrington <hello@loosenthedark.tech>
2021-10-08 20:43:43 +0100

    Initial commit

---
+++ controllers/default.js
@@ -0,0 +1,60 @@
+const Pty = require('node-pty');
+const fs = require('fs');
+
+exports.install = function () {
+
+    ROUTE('/');
+    WEBSOCKET('/', socket, ['raw']);
+
+};
+
+function socket() {
+
+    this.encodedecode = false;
+    this.autodestroy();
+
+    this.on('open', function (client) {
+
+        // Spawn terminal
+        client.tty = Pty.spawn('python3', ['run.py'], {
+            name: 'xterm-color',
+            cols: 80,
+            rows: 24,
+            cwd: process.env.PWD,
+            env: process.env
+        });
+
+        client.tty.on('exit', function (code, signal) {
+            client.tty = null;
+            client.close();
+            console.log("Process killed");
+        });
+
+        client.tty.on('data', function (data) {
+            client.send(data);
+        });
+
+    });
+
+    this.on('close', function (client) {
+        if (client.tty) {
+            client.tty.kill(9);
+            client.tty = null;
+            console.log("Process killed and terminal unloaded");
+        }
+    });
+
+    this.on('message', function (client, msg) {
+        client.tty && client.tty.write(msg);
+    });
+}
+
+if (process.env.CREDS != null) {
+    console.log("Creating creds.json file.");
+    fs.writeFile('creds.json', process.env.CREDS, 'utf8', function (err) {
+        if (err) {
+            console.log('Error writing file: ', err);
+            socket.emit("console_output", "Error saving credentials: " + err);
+        }
+    });
+}
---
+++ run.py
@@ -0,0 +1,3 @@
+# Your code goes here.
+# You can delete these comments, but do not change the name of this file
+# Write your code to expect a terminal of 80 characters wide and 24 rows high
