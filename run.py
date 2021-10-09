5142b9a222e2eca501d0b1c2ec158b488b903b25
Paul Harrington <hello@loosenthedark.tech>
2021-10-09 15:01:56 +0100

    Advance text retrieval functionality with two helper fns

--- run.py
+++ run.py
@@ -4,6 +4,8 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 from __future__ import print_function, unicode_literals
 
+import sys
+
 from pprint import pprint
 
 import regex
@@ -32,21 +34,21 @@ class InputValidator(Validator):
                 cursor_position=len(document.text))  # Move cursor to end
 
 
-print('Hi, welcome to Python Pizza')
+print('Welcome to The Zen of Typing - the only place you can improve your typing speed and brush up on some programming essentials at the same time!')
 
 questions = [
     {
         'type': 'list',
         'name': 'text',
-        'message': 'Choose a text:',
+        'message': 'Please choose a text:',
         'choices': ['dry', 'jokes', 'oop', 'python', 'sunscreen', 'zen'],
         'filter': lambda val: val.lower()
     },
     {
         'type': 'list',
-        'name': 'text',
-        'message': 'How many lines do you want?',
-        'choices': ['1', '3', '5', 'The whole thing!'],
+        'name': 'lines',
+        'message': 'How many lines would you like?',
+        'choices': ['1', '3', '5', 'Give me the whole thing!'],
         'filter': lambda val: val.lower()
     },
     {
@@ -64,10 +66,23 @@ questions = [
     }
 ]
 
-# def choose_text(self):
-#     pass
+def choose_text(text):
+    file = open(f'texts/{text}.txt').read()
+    return file
+
+def get_lines_for_typing(text, lines):
+    lines_for_typing = text.split('\n')
+    if lines == 'give me the whole thing!':
+        return '\n'.join(lines_for_typing)
+    print(lines)
+    return '\n'.join(lines_for_typing[:int(lines)])
 
 if __name__ == '__main__':
     answers = prompt(questions)
-    print('Order receipt:')
-    pprint(answers)  # use the answers as input for your app
+    chosen_text = answers['text']
+    num_of_lines = answers['lines']
+    print('You have chosen to type:')
+    print(f"{num_of_lines} lines from {chosen_text}...")
+    TEXT_FOR_TYPING = choose_text(chosen_text)
+    TEXT_FOR_TYPING = get_lines_for_typing(TEXT_FOR_TYPING, num_of_lines)
+    print(TEXT_FOR_TYPING)
