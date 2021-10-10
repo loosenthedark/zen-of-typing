9efd3928d4b576cd5ee6be3fd4fa59293ae68c1c
Paul Harrington <hello@loosenthedark.tech>
2021-10-10 12:07:51 +0100

    Instantiate main TypingText class with __init__ constructor method

--- README.md
+++ README.md
@@ -9,3 +9,9 @@
 pyjokes: "One line jokes for programmers (jokes as a service)" - [GitHub](https://github.com/pyjokes/pyjokes) | [Docs](https://pyjok.es/install/)
 
 ['textwrap — Text wrapping and filling'](https://docs.python.org/3/library/textwrap.html) (Python Docs)
+
+['Ninety-ninety rule'](https://en.wikipedia.org/wiki/Ninety%E2%80%93ninety_rule) (Wikipedia)
+
+### Potential Future Features:
+
+- ask user to enter email and send them a breakdown of their typing speed/accuracy (via smtplib module)?
--- run.py
+++ run.py
@@ -39,7 +39,7 @@ class InputValidator(Validator):
 
 
 print('')
-print('Welcome to The Zen of Typing!!!')
+print('Welcome to The Zen of Typing!')
 print('')
 print('The only place you can improve your typing speed and')
 print('brush up on some programming principles at the same time...')
@@ -76,21 +76,53 @@ questions = [
 ]
 
 def choose_text(text):
-    if text == 'jokes':
-        jokes = pyjokes.get_jokes()
-        random.shuffle(jokes)
-        jokes = jokes[:10]
-        file = '\n'.join(jokes)
-        return file
-    else:
-        file = open(f'texts/{text}.txt').read()
-        return file
+    '''
+    Retrieve typing text content based on selection made by user from multiple-choice menu options.
+    Since files are being loaded from disc/API request, which has the potential for problems
+     that might trigger an exception, code here should be wrapped in a try/except block
+    '''
+    try: # load content from file/API
+        if text == 'jokes':
+            jokes = pyjokes.get_jokes()
+            random.shuffle(jokes)
+            jokes = jokes[:10]
+            file = '\n'.join(jokes)
+        else:
+            file = open(f'texts/{text}.txt').read()
+    except Exception as ex:
+        # use a default/fallback quote in case there's
+        # a problem with loading content from file/API
+        file = ('The first 90 percent of the code accounts for'
+        ' the first 90 percent of the development time. '
+        'The remaining 10 percent of the code accounts for'
+        ' the other 90 percent of the development time.')
+
+    return file
 
 def get_lines_for_typing(text, lines):
-    lines_for_typing = text.split('\n')
-    if lines == 'give me the whole thing!':
-        return lines_for_typing
-    return lines_for_typing[:int(lines)]
+    '''
+    Customise length of typing text content based on user's preference:
+     return either 1, 3 or 5 lines; or else the entire body of stored text.
+    '''
+    try:
+        lines_for_typing = text.split('\n')
+        if lines == 'give me the whole thing!':
+            return lines_for_typing
+        return lines_for_typing[:int(lines)]
+    except Exception as ex:
+        print(ex)
+
+class TypingText:
+
+    def __init__(self):
+        self.reset = True
+        self.started = self.finished = self.running = False
+        self.text_typed = self.text_to_be_typed = ''
+        self.start_time = self.total_time = self.typing_accuracy = self.wpm = 0
+        self.score = f'Time: {self.total_time} | Accuracy: {self.typing_accuracy}% | WordsPerMinute: {self.wpm}'
+
+    def format_text(self):
+        pass
 
 if __name__ == '__main__':
     answers = prompt(questions)
@@ -101,7 +133,11 @@ if __name__ == '__main__':
     print('')
     print(f"{num_of_lines} lines from {chosen_text}...")
     print('')
+    # test exception handling functionality within choose_text fn
+    # print(choose_text(text=None))
     TEXT_FOR_TYPING = choose_text(chosen_text)
+    # test exception handling functionality within get_lines_for_typing fn
+    # print(get_lines_for_typing(chosen_text, 'abc'))
     TEXT_FOR_TYPING = get_lines_for_typing(TEXT_FOR_TYPING, num_of_lines)
     for line in TEXT_FOR_TYPING:
         print(textwrap.fill(line, width=80))
